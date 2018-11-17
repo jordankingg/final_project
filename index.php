19986d799c5161afe1d0f6d555d143612059d8ac
Jordan <jordanking@mymail.mines.edu>
2018-11-16 18:17:42 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+	<title>ISP Project</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/2.4.3/fabric.min.js"></script>
+</head>
+<body>
+<h1 id="title">Home</h1>
+<!-- Choose Login or Create Account -->
+<div id="page1">
+	<div id = "login_area">
+		<h1>Log In</h1>
+		<input id="username" type="text" placeholder="Enter Username"><br>
+		<button id="login">Enter Password</button>
+	</div>
+	<div id ="create_area">
+		<h1> Create an Account </h1>
+		<button id="create">Create Account</button>
+	</div>
+</div>
+</body>
+</html>
